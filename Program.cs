cd93f4dd59f4e8566adf1f50ba9242b2ba850999
pvquang89 <phamvanquang12042002@gmail.com>
2024-06-28 14:54:53 +0700

    example with factory method

--- Program.cs
+++ Program.cs
@@ -1,33 +1,69 @@
-﻿﻿internal class Program
+﻿using System.Data.Common;
+using System.Runtime.CompilerServices;
+
+internal class Program
 {
-    public class Singleton
+
+    public abstract class Animal
     {
-        // Tạo một biến static để lưu trữ thể hiện duy nhất của lớp Singleton
-        private static Singleton instance;
-
-        // Không cho tạo thể hiện từ bên ngoài
-        private Singleton() { }
-
-        // Cung cấp một phương thức static để lấy thể hiện duy nhất của lớp Singleton
-        //(vì ko thể tạo obj nên để static)
-        public static Singleton getInstance()
+        public abstract void Speak();
+    }
+    public class Cat : Animal
+    {
+        public override void Speak()
         {
-            if (instance == null)
-                instance = new Singleton();
-            return instance;
-        }
-        public void showMessage()
-        {
-            System.Console.WriteLine("hello");
+            System.Console.WriteLine("Meo meo meo .....");
         }
     }
 
+    public class Dog : Animal
+    {
+        public override void Speak()
+        {
+            System.Console.WriteLine("Gau Gau Gau .....");
+        }
+    }
+
+    public abstract class AnimalFactory
+    {
+        public abstract Animal CreateAnimal();
+
+        public void SpeakAnimal()
+        {
+            Animal animal = CreateAnimal();
+            animal.Speak();
+        }
+    }
+
+    public class CatFactory : AnimalFactory
+    {
+        public override Animal CreateAnimal()
+        {
+            return new Cat();
+        }
+    }
+
+    public class DogFactory : AnimalFactory
+    {
+        public override Animal CreateAnimal()
+        {
+            return new Dog();
+        }
+    }
     private static void Main(string[] args)
     {
-        //Sai vì không có hàm tạo public
-        // Singleton s = new Singleton();
-        //Chỉ có thể tạo thông qua chính lớp đấy gọi tới phương thức static đã định sẵn
-        Singleton s = Singleton.getInstance();
-        s.showMessage();
+        //tạo nhà máy mèo
+        AnimalFactory catFactory = new CatFactory();   
+        //tạo nhà máy chó
+        AnimalFactory dogFactory = new DogFactory();
+    
+        //tạo đối tượng từ method của nhà máy
+        //tạo mà không cần biết rõ nó là loại nào
+        //tuân thủ nguyên tắc Solid : Open closed principle
+        Animal cat = catFactory.CreateAnimal();
+        Animal dog = dogFactory.CreateAnimal(); 
+        //
+        cat.Speak();
+        dog.Speak();
     }
 }
